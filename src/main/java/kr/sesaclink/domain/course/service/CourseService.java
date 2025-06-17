0636fba1dfbe0794678d085ca0a9c4fb2481def8
KwangJin95 <rhkdwls95@naver.com>
2025-06-17 09:46:20 +0900

    Update CourseService.java

--- src/main/java/kr/sesaclink/domain/course/service/CourseService.java
+++ src/main/java/kr/sesaclink/domain/course/service/CourseService.java
@@ -6,15 +6,8 @@ import kr.sesaclink.domain.course.dto.CourseSearchDTO;
 import java.util.List;
 
 public interface CourseService {
-    // 캠퍼스별 강좌 목록 조회
-    List<CourseSearchDTO> getCourseListByCampus(Integer campusNo);
 
     // 회원 등록 강좌 목록 조회
     List<CourseDTO> getCourseListByUserNo(Long userNo);
 
-    // 새싹 회원 등록 강좌 목록 조회
-    List<CourseDTO> getCourseListByEmail(String email);
-
-    // 강좌 반환
-    CourseDTO getCourseByCourseNo(Integer courseNo);
 }
